47efac429ada313d0176c421f433a7dbb9ac4f65
vikram1503 <vikram15031989@gmail.com>
2024-06-14 20:08:10 +0530

    sg

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -223,3 +223,12 @@ resource "aws_security_group_rule" "web_alb_public_https" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.web_alb.sg_id
 }
+#added as part of jenkins cicd
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
